bccdbe5e1b509122036cdaa512700d87b2ce4fb2
Paul Istratoaie <p4ul.gg@gmail.com>
2021-04-08 00:14:04 +0100

    style: add shadow to DOM objects to create depth

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
 (function () {
     'use strict'
 
-    document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function () {
-        document.querySelector('.offcanvas-collapse').classList.toggle('open')
+    document.querySelector('#navbar-toggler').addEventListener('click', function () {
+        document.querySelector('.offcanvas-collapse').classList.toggle('open');
+        document.querySelector('#navbar-toggler .bi').classList.toggle('bi-x');
     })
 })()
