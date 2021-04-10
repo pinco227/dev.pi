2de4708a1aeb7a1138319101f8b7df3d348322bf
Paul Istratoaie <p4ul.gg@gmail.com>
2021-04-11 00:28:33 +0100

    feat(footer): add social buttons to footer and make offcanvas menu close with every menu click

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,10 @@
 (function () {
     'use strict'
 
-    document.querySelector('#navbar-toggler').addEventListener('click', function () {
-        document.querySelector('.offcanvas-collapse').classList.toggle('open');
-        document.querySelector('#navbar-toggler .bi').classList.toggle('bi-x');
-    })
+    document.querySelectorAll('[data-toggle="offcavas"]').forEach(item => {
+        item.addEventListener('click', () => {
+            document.querySelector('.offcanvas-collapse').classList.toggle('open');
+            document.querySelector('#navbar-toggler .bi').classList.toggle('bi-x');
+        })
+    });
 })()
