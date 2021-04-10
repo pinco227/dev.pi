70602365e2ea7a7f8624b6bc9667463c005cf211
Paul Istratoaie <p4ul.gg@gmail.com>
2021-04-10 22:50:34 +0100

    feat: create Breadcrumbs template and add to relevant pages
    using Flask-Breadcrumbs library

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for, Markup)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from flask_breadcrumbs import Breadcrumbs, register_breadcrumb
 from werkzeug.security import check_password_hash
 from werkzeug.utils import secure_filename
 from datetime import date
@@ -12,6 +13,9 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+
+# Initialize Flask-Breadcrumbs
+Breadcrumbs(app=app)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -33,6 +37,7 @@ def context_processor():
 
 @app.route("/home")
 @app.route('/')
+@register_breadcrumb(app, '.', 'Home')
 def home():
     skills = list(mongo.db.skills.find())
     education = list(mongo.db.education.find())
@@ -42,25 +47,51 @@ def home():
 
 
 @app.route('/portfolio')
+@register_breadcrumb(app, '.portfolio', 'Portfolio')
 def portfolio():
     projects = list(mongo.db.projects.find())
     return render_template("portfolio.html", projects=projects)
 
 
+def view_project_dlc(*args, **kwargs):
+    """Get project details from requested url args
+
+    Returns:
+        dict: text to be displayed into breadcrumb (Project title)
+    """
+    slug = request.view_args['project']
+    project = mongo.db.projects.find_one({"slug": slug})
+    return [{'text': project['title']}]
+
+
 @app.route('/portfolio/<project>')
+@register_breadcrumb(app, '.portfolio.project', '', dynamic_list_constructor=view_project_dlc)
 def get_project(project):
     project = mongo.db.projects.find_one({"slug": project})
     return render_template("project.html", project=project)
 
 
 @app.route('/blog')
+@register_breadcrumb(app, '.blog', 'Blog')
 def blog():
     blogs = list(mongo.db.blogs.find())
     # today = date.today().strftime("%B %d, %Y")
     return render_template("blog.html", blogs=blogs)
 
 
+def view_blog_dlc(*args, **kwargs):
+    """Get blg post details from requested url args
+
+    Returns:
+        dict: text to be displayed into breadcrumb (Blog title)
+    """
+    slug = request.view_args['post']
+    post = mongo.db.blogs.find_one({"slug": slug})
+    return [{'text': post['title']}]
+
+
 @app.route('/blog/<post>')
+@register_breadcrumb(app, '.blog.post', '', dynamic_list_constructor=view_blog_dlc)
 def get_post(post):
     post = mongo.db.blogs.find_one({"slug": post})
     return render_template("blog-post.html", post=post)
