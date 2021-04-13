86e33c8ba25d23b2a4e42047af1dba6be9afcef0
Paul Istratoaie <p4ul.gg@gmail.com>
2021-04-13 23:38:10 +0100

    feat(admin): add logout functionality and wire up button from admin panel

--- app.py
+++ app.py
@@ -146,6 +146,15 @@ def login():
     return render_template("admin/login.html")
 
 
+@app.route("/admin/logout")
+def logout():
+    if session.get("user"):
+        session.pop("user")
+
+    flash("You have been logged out")
+    return redirect(url_for("home"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
